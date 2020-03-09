779c04c974aa934ecfaacde88b6363d99fba34d4
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2020-03-09 17:27:29 +0100

    add default value

--- db/migrate/20200306151046_add_admin_field_to_user.rb
+++ db/migrate/20200306151046_add_admin_field_to_user.rb
@@ -1,7 +1,7 @@
 class AddAdminFieldToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :admin, :boolean, null: false, default: false
-    add_column :users, :username, :string, null: false
+    add_column :users, :username, :string, null: false, default: ""
 
     add_index :users, :username, unique: true
   end
