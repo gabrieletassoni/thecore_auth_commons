f3fdd77efe020fe7f530363bb5da98397beb0d40
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2020-04-27 14:23:07 +0200

    Add HttpAutoLocale

--- lib/thecore_auth_commons.rb
+++ lib/thecore_auth_commons.rb
@@ -3,6 +3,7 @@ require "thecore_auth_commons/engine"
 require 'devise'
 require 'cancancan'
 require 'kaminari'
+require 'http_accept_language'
 
 module ThecoreAuthCommons
   # Your code goes here...
