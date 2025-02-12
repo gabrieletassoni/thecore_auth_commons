91a6075d7d4885163b261e83c102b03ea66be331
Gabriele Tassoni <g.tassoni@bancolini.com>
2025-02-12 10:49:49 +0000

    Debug api request auth chain

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This is part of Thecore framework: https://github.com/gabrieletassoni/thecore/tree/release/3
+
+It exposes two Env vars to chage the behaviour of Devise at startup:
+
+- MIN_PASSWORD_LENGTH: the minimum length of the password (default: 8)
+- SESSION_TIMEOUT_IN_MINUTES: the session timeout in minutes (default: 30)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :rememberable, :validatable
+    # Get the minimum password length from the Environemnt or set it to 8
+    devise :database_authenticatable, :rememberable, :trackable, :timeoutable, :validatable, password_length: ENV.fetch('MIN_PASSWORD_LENGTH', 8).to_i..128, timeout_in: ENV.fetch('SESSION_TIMEOUT_IN_MINUTES', 31).to_i.minutes
 end
