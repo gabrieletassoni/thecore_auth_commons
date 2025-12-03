b7bd0ccb305ec57d1c716b5a92c93b3e673c4abe
Gabriele Tassoni <g.tassoni@bancolini.com>
2025-12-03 10:59:37 +0000

    Skip validation in aligning user

--- lib/thecore_auth_commons.rb
+++ lib/thecore_auth_commons.rb
@@ -1,34 +1,34 @@
-require 'devise'
-require 'cancancan'
-require 'kaminari'
-require 'activerecord-nulldb-adapter'
+require "devise"
+require "cancancan"
+require "kaminari"
+require "activerecord-nulldb-adapter"
 require "thecore_settings"
 require "net/ldap"
-require 'omniauth'
-require 'omniauth-google-oauth2'
-require 'omniauth-entra-id'
+require "omniauth"
+require "omniauth-google-oauth2"
+require "omniauth-entra-id"
 
 require "thecore_auth_commons/engine"
 
 require "thecore/seed"
 
 module ThecoreAuthCommons
-  def self.oauth_vars? 
-     entra_id_vars? || google_oauth2_vars?
+  def self.oauth_vars?
+    entra_id_vars? || google_oauth2_vars?
   end
 
   def self.entra_id_vars?
-    ENV['ENTRA_CLIENT_ID'].present? && ENV['ENTRA_CLIENT_SECRET'].present? && ENV['ENTRA_TENANT_ID'].present?
+    ENV["ENTRA_CLIENT_ID"].present? && ENV["ENTRA_CLIENT_SECRET"].present? && ENV["ENTRA_TENANT_ID"].present?
   end
 
   def self.google_oauth2_vars?
-    ENV['GOOGLE_CLIENT_ID'].present? && ENV['GOOGLE_CLIENT_SECRET'].present?
+    ENV["GOOGLE_CLIENT_ID"].present? && ENV["GOOGLE_CLIENT_SECRET"].present?
   end
 
   # Controlla se l'utente esiste, altrimenti lo crea con una password casuale
   # e lo restituisce. Se l'utente esiste già, lo restituisce senza modificarlo.
 
-  def self.check_user email, name, surname, provider
+  def self.check_user(email, name, surname, provider)
     u = User.find_or_initialize_by(email: email)
     u.name = name
     u.surname = surname
@@ -54,8 +54,8 @@ module ThecoreAuthCommons
         auth: {
           method: :simple,
           username: server.admin_user,
-          password: server.admin_password
-        }
+          password: server.admin_password,
+        },
       )
 
       unless ldap.bind
@@ -81,7 +81,7 @@ module ThecoreAuthCommons
     puts "== Completato. Utenti importati: #{imported_count} =="
   end
   # Your code goes here...
-  def self.align_user email, entry, server_id
+  def self.align_user(email, entry, server_id)
     user = User.find_or_initialize_by(email: email)
     user.auth_source = "ldap #{server_id}"
 
@@ -96,33 +96,33 @@ module ThecoreAuthCommons
     entry[:memberOf].each do |group|
       group_name = group.split(",").first.split("=").last
       # Se il gruppo è un admin, assegna il ruolo admin
-      is_admin = true if [ "Administrators", "Domain Admins", "Schema Admins", "Enterprise Admins", "admins", "administrators" ].include?(group_name)
-      
+      is_admin = true if ["Administrators", "Domain Admins", "Schema Admins", "Enterprise Admins", "admins", "administrators"].include?(group_name)
+
       role = Role.find_or_create_by(name: group_name)
       user.roles << role unless user.roles.include?(role)
     end
 
     user.admin = is_admin if user.respond_to?(:admin)
     # Se l'utente è nuovo o ha cambiato qualcosa, salvalo
-    puts "Cannot save user #{email} with errors: #{user.errors.full_messages.join(", ")}" unless user.save # if user.new_record? || user.changed? || user.roles_changed?
+    puts "Cannot save user #{email} with errors: #{user.errors.full_messages.join(", ")}" unless user.save(:validate => false) # if user.new_record? || user.changed? || user.roles_changed?
     user
   end
 
   def self.generate_secure_password(length = 20)
-    raise ArgumentError, 'Length must be at least 4' if length < 4
+    raise ArgumentError, "Length must be at least 4" if length < 4
 
     # Caratteri da cui attingere
-    lowercase = ('a'..'z').to_a
-    uppercase = ('A'..'Z').to_a
-    numbers   = ('0'..'9').to_a
-    symbols   = ['!', '@', '#', '$', '%', '&', '*', '?', '-', '_', '+', '=']
+    lowercase = ("a".."z").to_a
+    uppercase = ("A".."Z").to_a
+    numbers = ("0".."9").to_a
+    symbols = ["!", "@", "#", "$", "%", "&", "*", "?", "-", "_", "+", "="]
 
     # Obbliga almeno un carattere da ogni gruppo
     password = [
       lowercase.sample,
       uppercase.sample,
       numbers.sample,
-      symbols.sample
+      symbols.sample,
     ]
 
     # Caratteri restanti scelti a caso tra tutti
@@ -132,5 +132,4 @@ module ThecoreAuthCommons
     # Mischia per evitare ordine prevedibile
     password.shuffle.join
   end
-
 end
--- lib/thecore_auth_commons/version.rb
+++ lib/thecore_auth_commons/version.rb
@@ -1,3 +1,3 @@
 module ThecoreAuthCommons
-  VERSION = "3.5.5".freeze
+  VERSION = "3.5.6".freeze
 end
